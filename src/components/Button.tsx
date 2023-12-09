4e682aec98a182e1bddd3d8ab5b584b28b99cf94
mnaibei <muchajulius@gmail.com>
2023-12-09 13:46:23 +0300

    Adjusting buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -30,7 +30,7 @@ const Button: React.FC<ButtonProps> = ({
     onClick,
     type,
     className: `text-white drop-shadow-2xl border-none py-4 px-8 rounded-lg text-[1.6rem] transition-all duration-200 flex flex-row gap-4 justify-center items-center cursor-pointer ${buttoncolor} ${buttonhovercolor} max-lg:text-3xl max-lg:py-8 max-lg:px-16 max-lg:rounded-xl
-    shadow-xl hover:scale-100 hover:-translate-y-2 hover:drop-shadow-xl transition-all duration-200 lg:w-max sm:w-2/3`,
+    shadow-xl hover:scale-100 hover:-translate-y-2 hover:drop-shadow-xl transition-all duration-200 lg:w-max sm:w-1/3`,
   };
 
   if (elementType === "input") {

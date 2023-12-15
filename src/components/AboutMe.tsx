2b8de927119f9c07aadcb46d0b0a91a1733f66c6
mnaibei <muchajulius@gmail.com>
2023-12-15 12:21:27 +0300

    Fixing lighthouse issues

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -60,7 +60,12 @@ const AboutMe: React.FC = () => {
         </div>
         <div className="flex flex-row justify-center gap-6 items-center pl-32 pr-32 mb-16  max-lg:flex-col max-lg:p-16 min-[1921px]:px-[45rem] min-[1921px]:mb-48">
           <article className="pl-60 max-lg:p-0">
-            <img src={me} alt={me} className="rounded-full" />
+            <img
+              src={me}
+              alt={me}
+              className="rounded-full h-fit w-fit"
+              loading="lazy"
+            />
           </article>
           <Swiper
             spaceBetween={100}
@@ -84,7 +89,7 @@ const AboutMe: React.FC = () => {
                     <img
                       src={paragraph.icon}
                       alt={paragraph.icon}
-                      className="w-24"
+                      className="w-24 h-fit"
                     />
                   </div>
                   <div>

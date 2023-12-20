ee6d4d908b4d84e64d93da265b603d7ed6c79809
mnaibei <muchajulius@gmail.com>
2023-12-20 11:04:03 +0300

    Comment out imprint and privacy in footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="bg-darkblue flex justify-around items-center gap-10 p-10 max-lg:flex-col max-lg:pb-48 relative z-[1]">
-      <div className="flex gap-10">
+      {/* <div className="flex gap-10">
         {FooterLinks.map((link, index) => (
           <Popup
             trigger={
@@ -55,7 +55,7 @@ const Footer: React.FC = () => {
             }
           </Popup>
         ))}
-      </div>
+      </div> */}
       <div className="socials flex gap-10 ">
         {sideBarLeftSocials.map((social: SocialLink, index: number) => (
           <Link
